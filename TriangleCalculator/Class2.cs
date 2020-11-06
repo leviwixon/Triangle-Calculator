69d675f3fb61ec9bf743305bff1b2f211ef67361
Levi <levi.wixon@trojans.dsu.edu>
2020-11-05 18:27:11 -0700

    Initial Github upload (hopefully)

---
+++ TriangleCalculator/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalculator
+{
+    class RightTriangle : Triangle
+    {
+        public static double maxAngle = 180.00;
+
+        private class triangleInfo
+        {
+            public double side1, side2, side3 = 0;
+            public double angle1, angle2, angle3 = 0;
+        }
+
+        public override double CalcSides()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double findArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double perimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private double Pythagorean()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TriangleCalculator/Class3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalculator
+{
+    class NonRight : Triangle
+    {
+        public static double maxAngle = 180.00;
+        public double angle1, angle2, angle3, side1, side2, side3 = 0;
+
+        public override double CalcSides()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double findArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double perimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void triangleType()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void twoTriangle()
+        {
+
+        }
+
+        private void oneTriangle()
+        {
+
+        }
+    }
+}
